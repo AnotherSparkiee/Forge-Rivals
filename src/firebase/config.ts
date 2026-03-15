8d2610f38d269cb09bb85fc6ac7ca89396efb37b
Lvl Up <dotatmleague@gmail.com>
2026-03-15 11:18:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2788872209-f7092",
+  "appId": "1:680539014052:web:3fafccf090ba18376d7731",
+  "apiKey": "AIzaSyDk-5jCdodI_Any76Jq2jiS3F_0DJBMnuo",
+  "authDomain": "studio-2788872209-f7092.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "680539014052"
+};
