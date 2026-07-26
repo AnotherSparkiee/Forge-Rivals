93ef758a95ec4e23f1eae1fb6dd046bf7d00a465
Lvl Up <dotatmleague@gmail.com>
2026-07-26 17:17:57 +0000

    Нужно ещё немного увеличить блок информации следующего соперника, а такж

--- src/app/lib/time-utils.ts
+++ src/app/lib/time-utils.ts
@@ -10,8 +10,8 @@ let syncPoint = {
 };
 
 const MSK_OFFSET = 3 * 60 * 60 * 1000;
-// Скорректировано на 12 февраля 2025, чтобы текущий день был близок к концу цикла
-export const GLOBAL_EPOCH_ISO = '2025-02-12T21:00:00Z'; 
+// Скорректировано на 11 февраля 2025, чтобы текущий день был концом цикла
+export const GLOBAL_EPOCH_ISO = '2025-02-11T21:00:00Z'; 
 
 export function setServerTime(serverMs: number) {
   if (typeof performance !== 'undefined') {
