47d5fc446d5f0ad1f52754c8ee94f6a452a112fd
Lvl Up <dotatmleague@gmail.com>
2026-07-26 16:22:17 +0000

    ## Error Type Runtime FirebaseError
    ## Error Message
    Expected first arg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { FirebaseClientProvider } from "@/firebase/client-provider";
